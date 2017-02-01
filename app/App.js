1d75fb519970cd55a72d813c08aa3c2ed2ef48b2
Fernando Pintado <ferpintado@gmail.com>
2017-02-01 16:31:34 -0700

    Open and closing button working along with dialog box

--- app/App.js
+++ app/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {cyan500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // injectTapEventPlugin();
@@ -13,8 +24,9 @@ import styles from './App.scss';
 class App extends React.Component {
 
   render() {
+    console.log(muiTheme);
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <BookingButton />
       </MuiThemeProvider>  
     )
--- app/components/booking_button/BookingButton.js
+++ app/components/booking_button/BookingButton.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 
 import CSSModules from 'react-css-modules';
 import styles from './BookingButton.scss';
 
 @CSSModules(styles, {allowMultiple: true})
 
-export default class BookingButton extends React.Component {
+class BookingButton extends Component {
 
-  clickEvent(e){
-    console.log("Clicked");
+  state = {
+    open: false
+  };
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+
+    // this.handleOpen.bind(this);
+    // this.handleClose.bind(this);
+  }
+    
+
+  handleOpen = () => {
+    this.setState({open: true});
+  }
+
+  handleClose = () => {
+    this.setState({open: false});
   }
 
   render() {
+    console.log(this.props.styles);
+
+    const style = {
+      // transform: 'translate(0px, 0px)'
+    }
     return (
-      <RaisedButton onClick={this.clickEvent} label="Book an appointment" backgroundColor="#F44336" styleName="booking-button"/>
+      <div>
+        <RaisedButton className={this.state.open ? this.props.styles.hidden : ''} onClick={this.handleOpen} label="Book an appointment" backgroundColor="#F44336" styleName="booking-button"/>
+        <Dialog
+          className={ this.props.styles.hiddenoverlay }
+          contentStyle={style}
+          modal={false} 
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </Dialog>
+        <RaisedButton className={!this.state.open ? this.props.styles.hidden : ''} onClick={this.handleClose} label="Close" backgroundColor="#F44336" styleName="booking-button"/>
+
+      </div>
     );
   }
 }
 
+export default BookingButton;
 
 
 
