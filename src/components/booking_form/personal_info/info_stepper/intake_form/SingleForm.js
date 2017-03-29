683fab6371f4c27da9be6958d472b6b5092a1be7
Fernando Pintado <ferpintado@gmail.com>
2017-03-29 07:40:03 -0500

    test for deployments

--- src/components/booking_form/personal_info/info_stepper/intake_form/SingleForm.js
+++ src/components/booking_form/personal_info/info_stepper/intake_form/SingleForm.js
@@ -67,7 +67,6 @@ class SingleForm extends React.Component {
                   item.value = formValues[completedItem];
                 break;
               }
-              console.log(item);
             }
         }
     }
