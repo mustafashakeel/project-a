b6e398eebd544547f700fdca6395a75e380d9e92
Amir Mousavi <amousavi@me.com>
2017-05-25 10:31:00 -0700

    confirmation time fixed #6799

--- src/components/thank_page/booking_summary/BookingSummary.js
+++ src/components/thank_page/booking_summary/BookingSummary.js
@@ -55,7 +55,11 @@ export class BookingSummary extends React.Component {
           <div className="bizAddress">{booking.location.address}</div>
           <div className="serviceName">{booking.service.name}</div>
           <div className="providerName">{booking.provider.name}</div>
-          <div className="bookingTime">{booking.timestamp.subtract(booking.service.duration, 'm').format("h:mm a")} - {booking.timestamp.add(booking.service.duration, 'm').format("h:mm a")}</div>
+          {booking.allowConfirmedBooking ?
+            <div className="bookingTime">{booking.timestamp.subtract(booking.service.duration, 'm').format("h:mm a")} - {booking.timestamp.add(booking.service.duration, 'm').format("h:mm a")}</div>
+            :
+            <div className="bookingTime">{booking.timestamp.format("h:mm a")} - {booking.timestamp.add(booking.service.duration, 'm').format("h:mm a")}</div>
+          }
           <div className="bookingDate">{booking.timestamp.format("dddd, MMMM Do YYYY")}</div>
             <SelectField
               id="addToCalendar"
